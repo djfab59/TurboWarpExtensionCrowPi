1746f8aecb36cbfc8cbcc15ece0aacd04d4cf478
djfab59 <50011073+djfab59@users.noreply.github.com>
2025-12-10 22:34:20 +0100

    delete index.js

--- extension/dht20.js
+++ extension/dht20.js
@@ -1,42 +1,47 @@
-class DHTModule {
-  getBlocks () {
-    return [
-      {
-        opcode: 'dhtTemp',
-        blockType: Scratch.BlockType.REPORTER,
-        text: 'température (°C)'
-      },
-      {
-        opcode: 'dhtHum',
-        blockType: Scratch.BlockType.REPORTER,
-        text: 'humidité (%)'
+(function (Scratch) {
+  'use strict';
+
+  class CrowPiDHT20 {
+    getInfo () {
+      return {
+        id: 'crowpidht20',
+        name: 'CrowPi DHT20',
+        blocks: [
+          {
+            opcode: 'dhtTemp',
+            blockType: Scratch.BlockType.REPORTER,
+            text: 'température (°C)'
+          },
+          {
+            opcode: 'dhtHum',
+            blockType: Scratch.BlockType.REPORTER,
+            text: 'humidité (%)'
+          }
+        ],
+        menus: {}
+      };
+    }
+
+    async dhtTemp () {
+      try {
+        const res = await fetch('http://127.0.0.1:3232/dht20/read');
+        const data = await res.json();
+        return (data && typeof data.temperature === 'number') ? data.temperature : 0;
+      } catch (e) {
+        return 0;
       }
-    ];
-  }
+    }
 
-  getMenus () {
-    return {};
-  }
-
-  async dhtTemp () {
-    try {
-      const res = await fetch('http://127.0.0.1:3232/dht20/read');
-      const data = await res.json();
-      return (data && typeof data.temperature === 'number') ? data.temperature : 0;
-    } catch (e) {
-      return 0;
+    async dhtHum () {
+      try {
+        const res = await fetch('http://127.0.0.1:3232/dht20/read');
+        const data = await res.json();
+        return (data && typeof data.humidity === 'number') ? data.humidity : 0;
+      } catch (e) {
+        return 0;
+      }
     }
   }
 
-  async dhtHum () {
-    try {
-      const res = await fetch('http://127.0.0.1:3232/dht20/read');
-      const data = await res.json();
-      return (data && typeof data.humidity === 'number') ? data.humidity : 0;
-    } catch (e) {
-      return 0;
-    }
-  }
-}
-
-module.exports = DHTModule;
+  Scratch.extensions.register(new CrowPiDHT20());
+})(Scratch);
--- extension/lcd.js
+++ extension/lcd.js
@@ -1,149 +1,154 @@
-class LCDModule {
-  getBlocks () {
-    return [
-      {
-        opcode: 'lcdOn',
-        blockType: Scratch.BlockType.COMMAND,
-        text: 'allumer le LCD'
-      },
-      {
-        opcode: 'lcdOff',
-        blockType: Scratch.BlockType.COMMAND,
-        text: 'éteindre le LCD'
-      },
-      {
-        opcode: 'lcdClear',
-        blockType: Scratch.BlockType.COMMAND,
-        text: 'effacer le LCD'
-      },
-      {
-        opcode: 'lcdWrite',
-        blockType: Scratch.BlockType.COMMAND,
-        text: 'afficher [TEXT] sur le LCD',
-        arguments: {
-          TEXT: {
-            type: Scratch.ArgumentType.STRING,
-            defaultValue: 'Hello CrowPi'
+(function (Scratch) {
+  'use strict';
+
+  class CrowPiLCD {
+    getInfo () {
+      return {
+        id: 'crowpilcd',
+        name: 'CrowPi LCD',
+        blocks: [
+          {
+            opcode: 'lcdOn',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'allumer le LCD'
+          },
+          {
+            opcode: 'lcdOff',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'éteindre le LCD'
+          },
+          {
+            opcode: 'lcdClear',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'effacer le LCD'
+          },
+          {
+            opcode: 'lcdWrite',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'afficher [TEXT] sur le LCD',
+            arguments: {
+              TEXT: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'Hello CrowPi'
+              }
+            }
+          },
+          {
+            opcode: 'lcdWriteLine',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'afficher [TEXT] sur ligne [LINE]',
+            arguments: {
+              TEXT: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'Hello'
+              },
+              LINE: {
+                type: Scratch.ArgumentType.NUMBER,
+                defaultValue: 1,
+                menu: 'lines'
+              }
+            }
+          },
+          {
+            opcode: 'lcdWriteBoth',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'ligne 1 [LINE1] | ligne 2 [LINE2]',
+            arguments: {
+              LINE1: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'Hello'
+              },
+              LINE2: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'World'
+              }
+            }
+          },
+          {
+            opcode: 'lcdScrollStart',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'défilement texte [TEXT] ligne [LINE] vitesse [SPEED] ms',
+            arguments: {
+              TEXT: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'Hello CrowPi'
+              },
+              LINE: {
+                type: Scratch.ArgumentType.NUMBER,
+                defaultValue: 1,
+                menu: 'lines'
+              },
+              SPEED: {
+                type: Scratch.ArgumentType.NUMBER,
+                defaultValue: 200
+              }
+            }
+          },
+          {
+            opcode: 'lcdScrollStop',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'arrêter le défilement'
+          }
+        ],
+        menus: {
+          lines: {
+            acceptReporters: false,
+            items: ['1', '2']
           }
         }
-      },
-      {
-        opcode: 'lcdWriteLine',
-        blockType: Scratch.BlockType.COMMAND,
-        text: 'afficher [TEXT] sur ligne [LINE]',
-        arguments: {
-          TEXT: {
-            type: Scratch.ArgumentType.STRING,
-            defaultValue: 'Hello'
-          },
-          LINE: {
-            type: Scratch.ArgumentType.NUMBER,
-            defaultValue: 1,
-            menu: 'lines'
-          }
-        }
-      },
-      {
-        opcode: 'lcdWriteBoth',
-        blockType: Scratch.BlockType.COMMAND,
-        text: 'ligne 1 [LINE1] | ligne 2 [LINE2]',
-        arguments: {
-          LINE1: {
-            type: Scratch.ArgumentType.STRING,
-            defaultValue: 'Hello'
-          },
-          LINE2: {
-            type: Scratch.ArgumentType.STRING,
-            defaultValue: 'World'
-          }
-        }
-      },
-      {
-        opcode: 'lcdScrollStart',
-        blockType: Scratch.BlockType.COMMAND,
-        text: 'défilement texte [TEXT] ligne [LINE] vitesse [SPEED] ms',
-        arguments: {
-          TEXT: {
-            type: Scratch.ArgumentType.STRING,
-            defaultValue: 'Hello CrowPi'
-          },
-          LINE: {
-            type: Scratch.ArgumentType.NUMBER,
-            defaultValue: 1,
-            menu: 'lines'
-          },
-          SPEED: {
-            type: Scratch.ArgumentType.NUMBER,
-            defaultValue: 200
-          }
-        }
-      },
-      {
-        opcode: 'lcdScrollStop',
-        blockType: Scratch.BlockType.COMMAND,
-        text: 'arrêter le défilement'
-      }
-    ];
+      };
+    }
+
+    lcdOn () {
+      this._post('/lcd/on', {});
+    }
+
+    lcdOff () {
+      this._post('/lcd/off', {});
+    }
+
+    lcdClear () {
+      this._post('/lcd/clear', {});
+    }
+
+    lcdWrite (args) {
+      this._post('/lcd/write', { text: args.TEXT });
+    }
+
+    lcdWriteLine (args) {
+      this._post('/lcd/line', {
+        line: Number(args.LINE),
+        text: args.TEXT
+      });
+    }
+
+    lcdWriteBoth (args) {
+      this._post('/lcd/both', {
+        line1: args.LINE1,
+        line2: args.LINE2
+      });
+    }
+
+    lcdScrollStart (args) {
+      this._post('/lcd/scroll/start', {
+        text: args.TEXT,
+        line: Number(args.LINE),
+        speed: Number(args.SPEED)
+      });
+    }
+
+    lcdScrollStop () {
+      this._post('/lcd/scroll/stop', {});
+    }
+
+    _post (path, data) {
+      fetch('http://127.0.0.1:3232' + path, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data)
+      });
+    }
   }
 
-  getMenus () {
-    return {
-      lines: {
-        acceptReporters: false,
-        items: ['1', '2']
-      }
-    };
-  }
-
-  lcdOn () {
-    this._post('/lcd/on', {});
-  }
-
-  lcdOff () {
-    this._post('/lcd/off', {});
-  }
-
-  lcdClear () {
-    this._post('/lcd/clear', {});
-  }
-
-  lcdWrite (args) {
-    this._post('/lcd/write', { text: args.TEXT });
-  }
-
-  lcdWriteLine (args) {
-    this._post('/lcd/line', {
-      line: Number(args.LINE),
-      text: args.TEXT
-    });
-  }
-
-  lcdWriteBoth (args) {
-    this._post('/lcd/both', {
-      line1: args.LINE1,
-      line2: args.LINE2
-    });
-  }
-
-  lcdScrollStart (args) {
-    this._post('/lcd/scroll/start', {
-      text: args.TEXT,
-      line: Number(args.LINE),
-      speed: Number(args.SPEED)
-    });
-  }
-
-  lcdScrollStop () {
-    this._post('/lcd/scroll/stop', {});
-  }
-
-  _post (path, data) {
-    fetch('http://127.0.0.1:3232' + path, {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify(data)
-    });
-  }
-}
-
-module.exports = LCDModule;
+  Scratch.extensions.register(new CrowPiLCD());
+})(Scratch);
